c3a2a318ae1e43af33788649b24ad803c91ca069
Roopam Paradkar <roopamparadkar@gmail.com>
2020-10-11 00:28:53 +0530

    change folder structure

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Tasker
 
-A todo application armed with User Authentication and Super admin role.
+A todo application armed with User Authentication using google.
 
 # Technology Stack
 
-Frontend Build on Angular 10 and backend on NodeJs and MongoDB.
+Frontend Build on Angular 10 and Firebase for the backend
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
